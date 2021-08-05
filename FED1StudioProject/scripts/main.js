7cf7aab737fb45a5bd424c80faa00a58bb3cf50a
Caleb Mitchell <calmitch14@gmail.com>
2021-08-05 15:58:02 -0500

    Add project files.

---
+++ FED1StudioProject/scripts/main.js
@@ -0,0 +1,20 @@
+﻿
+/*********************************************
+                Hamburger Menu
+**********************************************/
+
+    //Toggle Hamburger Menu Script
+function toggleNav() {
+    let navList = document.getElementById('nav-list');
+    navList.classList.toggle('expanded');
+
+    //Toggle Hamburger button/close button
+    let btnMenu = document.getElementById('menu-btn');
+
+    if (navList.classList.contains('expanded')) {
+        btnMenu.innerHTML = "&times;";
+    }
+    else {
+        btnMenu.innerHTML = "&#9776;";
+    }
+}
